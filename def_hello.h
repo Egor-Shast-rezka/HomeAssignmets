60ba7e90c7618404d7f6febe93621dfa8ab909ea
Egor-Shast-rezka <st129457@student.spbu.ru>
2024-09-29 15:54:29 +0300

    comments added, code bugs fixed

--- def_hello.h
+++ def_hello.h
@@ -1,3 +1,7 @@
+/* Egor Shastin st129457@student.spbu.ru
+    This is the file header
+*/
+
 #ifndef MAIN_H
 #define MAIN_H
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include "def_hello.h"
+/* Egor Shastin st129457@student.spbu.ru
+    This is main function, that run program
+*/
 
-// based function for run code
+#include "def_hello.h"
 
 int main(){
 	print_hello();
