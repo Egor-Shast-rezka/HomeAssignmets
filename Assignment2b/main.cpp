0a16c52ac07f5fe1b7472bc60cee13d672b5ac20
Egor-Shast-rezka <st129457@student.spbu.ru>
2024-10-09 21:47:28 +0300

    no separate library is used for splitting by spaces

--- Assignment2b/main.cpp
+++ Assignment2b/main.cpp
@@ -5,40 +5,76 @@
 #include <iostream>
 #include <string>
 #include <stack>
-#include <sstream>
-
 
 int main() {
+
     // receiving data from user
     std::string str;
-    std::cout << "Write your exspression: ";
+    std::cout << "Write your expression: ";
     std::getline(std::cin, str);
 
-    // split by spaces
-    std::istringstream iss(str);
-    std::string elem;
-    
     // main function, calculate reverse polish notation
     std::stack<int> stack;
+    std::string elem;
+
+    for (unsigned long i = 0; i < str.length(); i++) {
     
-    while (iss >> elem) {
-    
-        // check, (elem == digit) or (elem == sign(+-*/) => calculate part of result)
+        // split by spaces
+        if (str[i] != ' ') {
+        
+            // collect the string up to the space
+            elem += str[i];
+        } else if (!elem.empty()) {
+        
+            // check, (elem == digit) or (elem == sign(+-*/) => calculate part of result)
+            bool isNum = true;
+            for (char num : elem) {
+                if (!std::isdigit(num)) {
+                    isNum = false;
+                    break;
+                }
+            }
+
+            // if digit => push in stack, else calculate part of result
+            if (isNum) {
+                int num = std::stoi(elem);
+                stack.push(num);
+            } else {
+            
+                // calculate part of result
+                int num1 = stack.top();
+                stack.pop();
+                int num2 = stack.top();
+                stack.pop();
+                if (elem == "+") {
+                    stack.push(num2 + num1);
+                } else if (elem == "-") {
+                    stack.push(num2 - num1);
+                } else if (elem == "*") {
+                    stack.push(num2 * num1);
+                } else if (elem == "/") {
+                    stack.push(num2 / num1);
+                }
+            }
+
+            elem = ""; // Clean elem 
+        }
+    }
+
+    // Process the last element (if there was no space at the end of the line)
+    if (!elem.empty()) {
         bool isNum = true;
-        for (char num : elem){
-            if (!std::isdigit(num)){
+        for (char num : elem) {
+            if (!std::isdigit(num)) {
                 isNum = false;
                 break;
             }
         }
-        
-        // if digit => push in stack, else calculate part of result
+
         if (isNum) {
             int num = std::stoi(elem);
             stack.push(num);
         } else {
-            
-            // calculate part of result
             int num1 = stack.top();
             stack.pop();
             int num2 = stack.top();
@@ -53,11 +89,12 @@ int main() {
                 stack.push(num2 / num1);
             }
         }
-        
     }
+
     // print result exspression
     std::cout << stack.top() << std::endl;
-    return 1;
+
+    return 0;
 }
 
 
