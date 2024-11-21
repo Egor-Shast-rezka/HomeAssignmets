81367c442054b22a01b39929c5e6cf13acd1075c
Egor-Shast-rezka <st129457@student.spbu.ru>
2024-11-21 22:14:39 +0300

    added test new virtual methods

--- Assignment5/main.cpp
+++ Assignment5/main.cpp
@@ -5,31 +5,68 @@
 */
 
 #include <iostream>
-#include <string> // only for getting data from user, not used in general code
+#include <string> // Only for getting data from user, not used in general code
 #include "Transformer.h"
 #include "Autobot.h"
 #include "Dinobot.h"
 #include "Decepticon.h"
 #include "Weapon.h"
+#include <vector> // Only for Assignment5
 
 //Main function
 int main()
 {
-    // Create first Transformer and create your strong
+    // All task for Autobot
     Autobot autobot;
-    autobot.setAmmo(100);
     autobot.fire();
     autobot.ultimate();
+    autobot.transform();
     
+    Transformer* newTransformer = &autobot;
+    newTransformer->fire();
+    newTransformer->ultimate();
+    newTransformer->transform();
+    
+    // These lines are comical because they repeat Autobot, this is done to shorten the output
+    /*
+    // All task for Decepticon
     Decepticon decepticon;
-    decepticon.setAmmo(100);
     decepticon.fire();
     decepticon.ultimate();
+    decepticon.transform();
     
+    Transformer* newTransformer_2 = &decepticon;
+    newTransformer_2->fire();
+    newTransformer_2->ultimate();
+    newTransformer_2->transform();
+    
+    // All task for Dinobot
     Dinobot dinobot;
-    dinobot.setAmmo(100);
     dinobot.fire();
     dinobot.ultimate();
+    dinobot.transform();
+    
+    Transformer* newTransformer_3 = &dinobot;
+    newTransformer_3->fire();
+    newTransformer_3->ultimate();
+    newTransformer_3->transform();
+    */
+    
+    Autobot aut1, aut2, aut3;
+    Decepticon dec1, dec2, dec3;
+    Dinobot din1, din2, din3;
+
+    std::vector<Transformer*> transformers; // Create new vector
+
+    transformers = {&dec1, &dec2, &dec3, &aut1, &aut2, &aut3, &din1, &din2, &din3};
+
+    for (Transformer* tr_mer : transformers) {
+        tr_mer->transform();
+        tr_mer->fire();
+        tr_mer->ultimate();
+        std::cout << std::endl;
+    }
+
     return 0;
 }
 
--- Assignment5/test_transformer.cpp
+++ Assignment5/test_transformer.cpp
@@ -11,6 +11,139 @@
 #include "Ability.h"
 #include "Weapon.h"
 
+// Tests for Assignment5
+
+// Test Autobot
+TEST(TransformerPolymorphismTest, AutobotVirtualTest) {
+    Autobot autobot;
+    Transformer* transformer = &autobot;
+
+    // Check Fire
+    testing::internal::CaptureStdout(); // Capture output to check what exactly was printed to the screen as a result of calling the fire() method
+    transformer->fire();
+    std::string output_1 = testing::internal::GetCapturedStdout(); // The captured output is saved into the string variable output.
+    EXPECT_NE(output_1.find("Autobot"), std::string::npos); // This macro checks that the string "Autobot" is present in the output variable.
+    
+    // Check ultimate
+    testing::internal::CaptureStdout();
+    transformer->ultimate();
+    std::string output_2 = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output_2.find("Autobot"), std::string::npos);
+    
+    //Check transform
+    testing::internal::CaptureStdout();
+    transformer->transform();
+    std::string output_3 = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output_3.find("Autobot"), std::string::npos);
+}
+
+// Test Decepticon
+TEST(TransformerPolymorphismTest, DecepticonVirtualTest) {
+    Decepticon decepticon;
+    Transformer* transformer = &decepticon;
+
+    // Check Fire
+    testing::internal::CaptureStdout(); // Capture output to check what exactly was printed to the screen as a result of calling the fire() method
+    transformer->fire();
+    std::string output_1 = testing::internal::GetCapturedStdout(); // The captured output is saved into the string variable output.
+    EXPECT_NE(output_1.find("Decepticon"), std::string::npos); // This macro checks that the string "Decepticon" is present in the output variable.
+    
+    // Check ultimate
+    testing::internal::CaptureStdout();
+    transformer->ultimate();
+    std::string output_2 = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output_2.find("Decepticon"), std::string::npos);
+    
+    // Check transform
+    testing::internal::CaptureStdout();
+    transformer->transform();
+    std::string output_3 = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output_3.find("Decepticon"), std::string::npos);
+}
+
+// Test Dinobot
+TEST(TransformerPolymorphismTest, DinobotVirtualTest) {
+    Dinobot dinobot;
+    Transformer* transformer = &dinobot;
+
+    // Check Fire
+    testing::internal::CaptureStdout(); // Capture output to check what exactly was printed to the screen as a result of calling the fire() method
+    transformer->fire();
+    std::string output_1 = testing::internal::GetCapturedStdout(); // The captured output is saved into the string variable output.
+    EXPECT_NE(output_1.find("Dinobot"), std::string::npos); // This macro checks that the string "Dinobot" is present in the output variable.
+    
+    // Check ultimate
+    testing::internal::CaptureStdout();
+    transformer->ultimate();
+    std::string output_2 = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output_2.find("Dinobot"), std::string::npos);
+    
+    // Check transform
+    testing::internal::CaptureStdout();
+    transformer->transform();
+    std::string output_3 = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output_3.find("Dinobot"), std::string::npos);
+}
+
+// Test all Transformer types (Autobot, Decepticon, Dinobot)
+TEST(TransformerPolymorphismTest, AllVirtualTest) {
+    Autobot a1, a2, a3;
+    Decepticon d1, d2, d3;
+    Dinobot t1, t2, t3;
+
+    // Create a vector of pointers to different Transformer objects
+    std::vector<Transformer*> transformers = {&t1, &t2, &t3, &a1, &a2, &a3, &d1, &d2, &d3};
+
+    // Iterate over each Transformer pointer and test virtual methods
+    for (Transformer* tr_mer : transformers) {
+        testing::internal::CaptureStdout();
+        tr_mer->transform();
+        std::string output_transform = testing::internal::GetCapturedStdout();
+
+        // Check if the output contains the correct class name
+        if (dynamic_cast<Autobot*>(tr_mer)) {
+            EXPECT_NE(output_transform.find("Autobot"), std::string::npos); // Check for "Autobot" in the output
+        } else if (dynamic_cast<Decepticon*>(tr_mer)) {
+            EXPECT_NE(output_transform.find("Decepticon"), std::string::npos); // Check for "Decepticon" in the output
+        } else {
+            EXPECT_NE(output_transform.find("Dinobot"), std::string::npos); // Check for "Dinibot" in the output
+        }
+
+        // Capture the output of fire()
+        testing::internal::CaptureStdout();
+        tr_mer->fire();
+        std::string output_fire = testing::internal::GetCapturedStdout();
+
+        // Check if the output contains the correct class name
+        if (dynamic_cast<Autobot*>(tr_mer)) {
+            EXPECT_NE(output_fire.find("Autobot"), std::string::npos); // Check for "Autobot" in the output
+        } else if (dynamic_cast<Decepticon*>(tr_mer)) {
+            EXPECT_NE(output_fire.find("Decepticon"), std::string::npos);  // Check for "Decepticon" in the output
+        } else {
+            EXPECT_NE(output_fire.find("Dinobot"), std::string::npos); // Check for "Dinibot" in the output
+        }
+
+        // Capture the output of ultimate()
+        testing::internal::CaptureStdout();
+        tr_mer->ultimate();
+        std::string output_ultimate = testing::internal::GetCapturedStdout();
+
+        // Check if the output contains the correct class name
+        if (dynamic_cast<Autobot*>(tr_mer)) {
+            EXPECT_NE(output_ultimate.find("Autobot"), std::string::npos); // Check for "Autobot" in the output
+        } else if (dynamic_cast<Decepticon*>(tr_mer)) {
+            EXPECT_NE(output_ultimate.find("Decepticon"), std::string::npos);  // Check for "Decepticon" in the output
+        } else {
+            EXPECT_NE(output_ultimate.find("Dinobot"), std::string::npos); // Check for "Dinibot" in the output
+        }
+    }
+}
+
+// These tests were used in previous tasks, they are committed for easy checking of the output in the console
+
+/*
+// Basic test for Assignment3
+
 // Tests for class Transformer
 TEST(TransformerTest, InitialValues)
 {
@@ -72,6 +205,8 @@ TEST(DinobotTest, DinobotCreation)
     EXPECT_EQ(dinobot.getAmmo(), 5); // check init ammo
     EXPECT_EQ(dinobot.getFuel(), 20); // check init fuel
 }
+
+// Test for Assignment4
 
 // Test overload operator
 TEST(AutobotTest, ComparisonOperators)
@@ -147,3 +282,4 @@ TEST(TransformerComparisonTest, DinobotVsAutobot)
     dinobot.setFirepowerDinobot(130); // Change for check
     EXPECT_EQ(dinobot, autobot); // Now they equal
 }
+*/
